1f44ec43c3ca52848dc509e7c40cec775c38bf9b
mburuxx <njihiamburu.3@gmail.com>
2022-12-12 12:20:04 +0300

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,11 @@
 /**
  * main - Print a string in unix without using puts or printf
  *
- * The write() function is a system call used to write data to a file descriptor.
- * It takes three arguments: a file descriptor (1 for the standard output), a pointer to the data to write,
- * and the number of bytes to write. The function returns the number of bytes written, or -1 if an error occurred.
+ * The write() function is a system call used to write data to a file
+ * descriptor. It takes three arguments: a file descriptor (1 for
+ * the standard output), a pointer to the data to write,
+ * and the number of bytes to write. The function returns the number
+ * of bytes written, or -1 if an error occurred.
  *
  * Return: 1 (exit_success)
  */
@@ -15,6 +17,7 @@
 int main(void)
 {
 	char name[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(1, name, strlen(name));
 	exit(1);
 }
