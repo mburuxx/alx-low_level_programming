d1685e6d43ddc88c951d64f6467ace7943d0ca7e
mburuxx <njihiamburu.3@gmail.com>
2022-12-13 10:42:56 +0300

    If a picture is worth a thousand words, a prototype is worth a thousand meetings

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10() - Prints the alphabet ten times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+
+		_putchar('\n');
+		i = i + 1;
+	}
+}
