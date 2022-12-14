aaf84a45e2d330bef264356e81a2f4ad717568af
mburuxx <njihiamburu.3@gmail.com>
2022-12-14 10:11:37 +0300

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10() - Prints the alphabet ten times
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase
  */
 
 void print_alphabet_x10(void)
