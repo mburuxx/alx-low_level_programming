3583909561f8bf137c178a2c7181dbd611db9fda
mburuxx <njihiamburu.3@gmail.com>
2022-12-15 16:56:33 +0300

    Isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,18 +2,16 @@
 
 /**
  * _isupper - Checks for uppercase characters
- * @c : Character to be checked
+ * @c: Character to be checked
  *
- * Return : 1 if character is uppercase and 0 otherwise
+ * Return: 1 if character is uppercase and 0 otherwise
  */
 
 int _isupper(int c)
 {
-
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 
 	else
 		return (0);
-
 }
