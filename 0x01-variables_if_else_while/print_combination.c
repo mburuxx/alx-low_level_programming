21cc5194d10d0e9630985373cb545d2e251a639c
mburuxx <njihiamburu.3@gmail.com>
2023-01-01 21:05:51 +0300

    Possible combiations of a single number

---
+++ 0x01-variables_if_else_while/print_combination.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int j;
+
+	for (j = '0'; j <= '9'; j++)
+	{
+		putchar(j);
+		if (j != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return(0);
+}
