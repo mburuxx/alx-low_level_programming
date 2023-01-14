ebe3c7397992837d4888bd1a7ef0f87d00954cb4
mburuxx <njihiamburu.3@gmail.com>
2023-01-15 00:19:49 +0300

    Fixed the for loop

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,34 +3,27 @@
 #include <ctype.h>
 
 /**
- * main - Adds two numbers
- * @argc: number of arguments
- * @argv: array of pointers to the arguments
- *
- * Return: Always 0
+ * main - Adds positive numbers followed by a new line.
+ * @argc: Number of comand line arguments.
+ * @argv: Arrays of comandline arguments.
+ * Return: 0.
  */
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
+	int count, j, sum = 0;
 
-	if (argc <= 1)
-		printf("0\n");
-	else
+	for (count = 1; count < argc; count++)
 	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[count][j] != '\0'; j++)
 		{
-			add = add + atoi(argv[i]);
-			for(j = 0; argv[i][j] != '\0'; j++)
+			if (!isdigit(argv[count][j]))
 			{
-				if (!isdigit(argv[i][j]))
-				{
-					printf("Error\n");
-					return (1);
-				}
+				printf("Error\n");
+				return (1);
 			}
 		}
-		printf("%d\n", add);
+		sum += atoi(argv[count]);
 	}
+	printf("%d\n", sum);
 	return (0);
 }
