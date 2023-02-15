f66a227f6a353c9578e72787edff726efa2e4d7a
mburuxx <njihiamburu.3@gmail.com>
2023-02-15 16:54:35 +0300

    Create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: is the name of the file to create.
+ * @text_content: is a NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int nlett;
+	int fwrite;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (nlett = 0; text_content[nlett]; nlett++)
+		;
+
+	fwrite = write(file, text_content, nlett);
+
+	if (fwrite == -1)
+		return (-1);
+
+	close(file);
+
+	return (1);
+}
